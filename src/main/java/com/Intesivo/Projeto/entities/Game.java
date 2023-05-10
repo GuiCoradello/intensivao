b8f561e32ae2ede4f4755069f57e6643a49ad3c4
GuiCoradello <coradello.silveira@gmail.com>
2023-05-10 01:06:33 -0300

    frist

---
+++ src/main/java/com/Intesivo/Projeto/entities/Game.java
@@ -0,0 +1,104 @@
+package com.Intesivo.Projeto.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+
+
+@Entity
+@Table(name="tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+    @Column()
+    private String title;
+    @Column(name = "game_Year")
+    private Integer year;
+    private String platform;
+    private String imgUrl;
+    private String ShortDescription;
+    private String longDescription;
+
+   public Game(){};
+
+    public Game(long id, String title, Integer year, String platform, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.ShortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return ShortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        ShortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Game game = (Game) obj;
+        return id == game.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
